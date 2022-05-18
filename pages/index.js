c83c274246acae0037f6380b3c2c6bd8f3c46bca
ankii2001 <ankitluthra2001@gmail.com>
2022-05-18 12:15:43 +0530

    abc

--- pages/index.js
+++ pages/index.js
@@ -43,10 +43,10 @@ export default function Home() {
 
         <div className="mt-8 flex flex-wrap items-center justify-center text-white gap-y-20 gap-x-24">
           
-        <div className="backdrop-blur-sm bg-ankit-100 w-[350px] h-[200px] lg:w-[350px] lg:h-[200px] shadow-xl shadow-black rounded-xl flex flex-row hover:scale-105 cursor-pointer ">
+        <div className="backdrop-blur-sm bg-ankit-100 w-[350px] h-[196px] lg:w-[350px] lg:h-[200px] shadow-xl shadow-black rounded-xl flex flex-row hover:scale-105 cursor-pointer ">
             <div className="basis-1/2 text-center font-light mt-2">
               <div className="text-3xl lg:mb-2 italic text-stone-900">Music</div>
-              <div class="text-xs text-center p-2 m-1 lg:p-y-1 lg:m-0 overflow-hidden text-stone-800">
+              <div class="text-xs text-left p-x-2 m-3 lg:text-center lg:p-2 lg:m-0 overflow-hidden text-stone-800">
                 Vocal or instrumental sounds combined in such a way as
                 to produce beauty of form, harmony, and expression of emotion.
               </div>
@@ -54,8 +54,8 @@ export default function Home() {
             <div className="flex items-center justify-center">
               <Image 
                 src="/dr-vineet-dahiya.jpg"
-                height={234}
-                width={234}
+                height={232}
+                width={232}
                 className="rounded-r-xl"
               />
             </div>
